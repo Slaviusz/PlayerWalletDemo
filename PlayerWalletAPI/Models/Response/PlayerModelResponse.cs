449f93e283963f7a1158d7ca4f5412a12fd36180
Slavomir Danas <slavomir.danas@outlook.com>
2020-10-06 15:58:00 +0200

    Refactoring - PlayerModelResponse
    Update XMLDoc
    Add Repeated (T/F) flag to response

--- PlayerWalletAPI/Models/Response/PlayerModelResponse.cs
+++ PlayerWalletAPI/Models/Response/PlayerModelResponse.cs
@@ -1,12 +1,15 @@
 using System;
+using System.ComponentModel;
 using AutoMapper;
 using AutoMapper.Configuration.Annotations;
 using PlayerWalletContext.Entities;
-
 #nullable disable
 
 namespace PlayerWalletAPI.Models.Response
 {
+    /// <summary>
+    /// Response model from PlayerController Get* methods
+    /// </summary>
     [AutoMap(typeof(Player))]
     public class PlayerModelResponse
     {
@@ -19,5 +22,8 @@ namespace PlayerWalletAPI.Models.Response
         /// <example>ExamplePlayerName</example>
         [SourceMember("PlayerName")]
         public string Name { get; set; }
+
+        [DefaultValue(false)]
+        public bool Repeated { get; set; }
     }
 }
