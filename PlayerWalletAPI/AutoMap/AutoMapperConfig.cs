ea5e5666d1771c556c9f4ffb4208325ab16c23f9
Slavomir Danas <slavomir.danas@outlook.com>
2020-10-06 15:59:14 +0200

    Add Automapper config for Player DTO and Models

--- PlayerWalletAPI/AutoMap/AutoMapperConfig.cs
+++ PlayerWalletAPI/AutoMap/AutoMapperConfig.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using PlayerWalletAPI.Models.Request;
 using PlayerWalletAPI.Models.Response;
 using PlayerWalletContext.Entities;
+#pragma warning disable 1591
 
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
@@ -9,10 +11,15 @@ namespace PlayerWalletAPI.AutoMap
 {
     public class AutoMapperConfig : Profile
     {
-        public void AutoMapping()
+        public AutoMapperConfig()
         {
-            // Define AutoMapper mapping configurations here
+            // Player DTO to ResponseModel
             CreateMap<Player, PlayerModelResponse>();
+
+            // PlayerAddRequest from PlayerController.Add() to Player DTO
+            var map1 = CreateMap<PlayerAddRequest, Player>();
+            map1.ForMember(player => player.Id, opt => opt.MapFrom(add => add.TransactionId));
+            map1.ForMember(player => player.PlayerName, opt => opt.MapFrom(add => add.PlayerName));
         }
     }
 }
