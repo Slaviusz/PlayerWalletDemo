28626c8e667ee46509d9629ebc8ca9f1870243f4
Slavomir Danas <slavomir.danas@outlook.com>
2020-10-06 22:16:48 +0200

    Tests - Add test for too short and too long Player name

--- PlayerWalletTests/PlayerAPITests_Add.cs
+++ PlayerWalletTests/PlayerAPITests_Add.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using PlayerWalletAPI.Models.Request;
 using PlayerWalletAPI.Models.Response;
 using Shouldly;
@@ -80,6 +82,58 @@ namespace PlayerWalletTests
             var response = await client.PostAsync("v1/Player/Add", requestModel);
 
             response.StatusCode.ShouldBe(HttpStatusCode.Conflict);
+        }
+
+        [Fact]
+        private async Task TestPlayerAdd_NameTooShort()
+        {
+            var client = _factory.CreateClient();
+
+            var json = JsonSerializer
+                .Serialize(new PlayerAddRequest
+                {
+                    TransactionId = Guid.NewGuid(),
+                    BirthDate = DateTime.Now.AddYears(-25),
+                    PlayerName = "a"
+                });
+
+            var requestModel = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("v1/Player/Add", requestModel);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            var problemDetails = await JsonSerializer
+                .DeserializeAsync<ValidationProblemDetails>(response.Content.ReadAsStreamAsync().Result);
+
+            problemDetails.Errors.ShouldContainKey("PlayerName");
+            problemDetails.Errors["PlayerName"].First().ShouldMatch("name must be between");
+        }
+
+        [Fact]
+        private async Task TestPlayerAdd_NameTooLong()
+        {
+            var client = _factory.CreateClient();
+
+            var json = JsonSerializer
+                .Serialize(new PlayerAddRequest
+                {
+                    TransactionId = Guid.NewGuid(),
+                    BirthDate = DateTime.Now.AddYears(-25),
+                    PlayerName = "abcdefghijklmnopqrstuvwxyz"
+                });
+
+            var requestModel = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("v1/Player/Add", requestModel);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+            var problemDetails = await JsonSerializer
+                .DeserializeAsync<ValidationProblemDetails>(response.Content.ReadAsStreamAsync().Result);
+
+            problemDetails.Errors.ShouldContainKey("PlayerName");
+            problemDetails.Errors["PlayerName"].First().ShouldMatch("name must be between");
         }
     }
 }
